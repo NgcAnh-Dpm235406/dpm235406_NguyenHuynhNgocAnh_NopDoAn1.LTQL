0b8f3a68434cf20944f55f5736a1605291f636f8
anh nguyen <anh_dpm235406@student.agu.edu.vn>
2026-04-18 03:58:31 +0700

    y

---
+++ DTO/frmbcHoaDon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DTO
+{
+    public partial class frmbcHoaDon : Form
+    {
+        public frmbcHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void frmbcHoaDon_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
